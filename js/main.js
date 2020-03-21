38998be3a47b58b776999582f4c57776214fb239
ali khan <m.alikhaninbox@gmail.com>
2020-03-21 19:18:57 +0500

    coverted css to scss

--- js/main.js
+++ js/main.js
@@ -1,14 +1,23 @@
 console.log('main.js connected')
 
 const limit = 550
+const downBtns = document.getElementsByClassName('down')
+
+// for (let i = 0; i < downBtns.length; i++) {
+//     downBtns[i].addEventListener('click', scrollDown)
+// }
 
 window.onscroll = function () {
+    console.log(window.scrollY)
     if (document.body.scrollTop > limit || document.documentElement.scrollTop > limit) {
         document.getElementById("nav").style.top = "0";
     } else {
         document.getElementById("nav").style.top = "-10vh";
     }
 }
-    ;
+    
+function scrollDown(y){
+    window.scrollTo(0,y)
+}
 
 
