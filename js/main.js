bb0aff444fc8ab7f8e197421db66d97007d3e7f0
ali khan <m.alikhaninbox@gmail.com>
2020-04-01 19:08:26 +0500

    backup
    backup april fool wala

--- js/main.js
+++ js/main.js
@@ -2,22 +2,35 @@ console.log('main.js connected')
 
 const limit = 550
 const downBtns = document.getElementsByClassName('down')
+const submit_input = document.getElementById('submit')
+const email_input = document.getElementById('email')
 
 // for (let i = 0; i < downBtns.length; i++) {
 //     downBtns[i].addEventListener('click', scrollDown)
 // }
+
+submit_input.addEventListener('click', function(){
+    let email = email_input.value
+    if(email === ''){
+        alert('Email is empty')
+    }
+})
+
 
 window.onscroll = function () {
     console.log(window.scrollY)
     if (document.body.scrollTop > limit || document.documentElement.scrollTop > limit) {
         document.getElementById("nav").style.top = "0";
     } else {
-        document.getElementById("nav").style.top = "-10vh";
+        document.getElementById("nav").style.top = "-20vh";
     }
 }
     
-function scrollDown(y){
-    window.scrollTo(0,y)
+function scrollDown(multiplier){
+    window.scrollTo(0, 682 * multiplier)
 }
+
+
+
 
 
